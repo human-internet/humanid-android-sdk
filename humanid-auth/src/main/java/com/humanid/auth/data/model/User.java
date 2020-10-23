5a4ba3e6f9a56b5c21d3c8c35578446d097a6f28
Cfakir3 <cfakir3@gatech.edu>
2020-10-23 16:03:47 -0400

    Finished User.java

--- humanid-auth/src/main/java/com/humanid/auth/data/model/User.java
+++ humanid-auth/src/main/java/com/humanid/auth/data/model/User.java
@@ -3,26 +3,49 @@ package com.humanid.auth.data.model;
 import androidx.annotation.NonNull;
 
 public class User {
-
+    /**
+     * String for calculating hash code.
+     */
     private String userHash;
 
+    /**
+     * String for identifying and comparing user hashes.
+     */
     private String exchangeToken;
 
+    /**
+     * Constructor.
+     * @param userHash : String for calculating hash code.
+     * @param exchangeToken : String for identifying and comparing user hashes.
+     */
     public User(@NonNull String userHash, @NonNull String exchangeToken) {
         this.userHash = userHash;
         this.exchangeToken = exchangeToken;
     }
 
+    /**
+     *
+     * @return : Returns userHash.
+     */
     @NonNull
     public String getUserHash() {
         return userHash;
     }
 
+    /**
+     *
+     * @return : Returns exchangeToken.
+     */
     @NonNull
     public String getExchangeToken() {
         return exchangeToken;
     }
 
+    /**
+     * Compares current userHash with userHash from Object o.
+     * @param o : userHash object that is use to compare.
+     * @return : Returns result of comparison.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -31,6 +54,10 @@ public class User {
         return userHash.equals(that.userHash);
     }
 
+    /**
+     *
+     * @return : Returns hash code of userHash.
+     */
     @Override
     public int hashCode() {
         int result = 1;
@@ -39,6 +66,11 @@ public class User {
         return result;
     }
 
+    /**
+     *
+     * @return : Returns print friendly string of object.
+
+     */
     @NonNull
     @Override
     public String toString() {

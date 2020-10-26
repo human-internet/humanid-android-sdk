d9be522f25658cdc7a93e4bb07614a25381b9848
derekchung10 <45410169+derekchung10@users.noreply.github.com>
2020-10-26 13:05:09 -0500

    Finished Resource, Status, and AbsentLiveData

--- humanid-auth/src/main/java/com/humanid/auth/util/livedata/AbsentLiveData.java
+++ humanid-auth/src/main/java/com/humanid/auth/util/livedata/AbsentLiveData.java
@@ -2,12 +2,22 @@ package com.humanid.auth.util.livedata;
 
 import androidx.lifecycle.LiveData;
 
+/**
+ * Null extension of the LiveData Class. Overrides main thread with value null
+ */
 public class AbsentLiveData<T> extends LiveData<T> {
 
+    /**
+     * Constructor. postValue(null) is called to post null to the main thread.
+     */
     private AbsentLiveData() {
         postValue(null);
     }
 
+    /**
+     * Creates a new AbsentLiveData object and returns it.
+     * @return AbsentLiveData<T>: the new object created by this function
+     */
     public static <T> AbsentLiveData<T> create() {
         return new AbsentLiveData<>();
     }

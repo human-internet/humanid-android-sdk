70aeae93ea3b08dfa1818e8de6e66f142141cdc6
Cfakir3 <cfakir3@gatech.edu>
2020-10-26 17:48:28 -0400

    Finished LoginResponse

--- humanid-auth/src/main/java/com/humanid/auth/data/source/remote/api/user/login/LoginResponse.java
+++ humanid-auth/src/main/java/com/humanid/auth/data/source/remote/api/user/login/LoginResponse.java
@@ -3,9 +3,16 @@ package com.humanid.auth.data.source.remote.api.user.login;
 import com.google.gson.annotations.SerializedName;
 
 public class LoginResponse {
+    /**
+     * Object storing a user hash and an exchange token, both used for logging in.
+     */
     @SerializedName("data")
     private LoginItem mLoginItem;
 
+    /**
+     *
+     * @return : Returns the current login item.
+     */
     public LoginItem getLoginItem() {
         return mLoginItem;
     }

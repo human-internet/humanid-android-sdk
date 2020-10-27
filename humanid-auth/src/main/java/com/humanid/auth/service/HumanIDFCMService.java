9a1aedfa30650663ad447d84ee3dd9af4d61abb4
Cfakir3 <cfakir3@gatech.edu>
2020-10-27 10:38:15 -0400

    HumanIDFCMService

--- humanid-auth/src/main/java/com/humanid/auth/service/HumanIDFCMService.java
+++ humanid-auth/src/main/java/com/humanid/auth/service/HumanIDFCMService.java
@@ -7,8 +7,15 @@ import com.humanid.auth.internal.DeviceIDManager;
 
 public class HumanIDFCMService extends FirebaseMessagingService {
 
+    /**
+     * Java string with value “HumanIDFCMService”.
+     */
     private final static String TAG = HumanIDFCMService.class.getSimpleName();
 
+    /**
+     Calls super.onNewToken(s), then DeviceID.getInstance(this).setNotificationID(s).
+     * @param s : String to set as Notification Id.
+     */
     @Override
     public void onNewToken(@NonNull String s) {
         super.onNewToken(s);
